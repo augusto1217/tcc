fcbd50528e055882d7bd314b5e6d1f78e91a8a34
Edson Alves <edsonalves@unb.br>
2015-06-17 22:15:37 -0300

    Revisão da Introdução.

--- CodingStyle/cods/utiltags.java
+++ CodingStyle/cods/utiltags.java
@@ -1,4 +1,4 @@
-/**Classe que coleta pacotes compactados de projetos open-source do repositório python
+/** Classe que coleta pacotes compactados de projetos open-source do repositório python
  * @author Osandi Augusto
  * @version 0.05
  * @since Release 01 da aplicação
